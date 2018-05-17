709cdc5d244718d40398d6384d1b25feb9a265d3
ninjawriter222 <devin.a.poe@gmail.com>
2018-05-16 20:43:47 -0700

    #4

---
+++ Dungeon Crawler/src/dungeon/crawler/model/Player.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dungeon.crawler.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author MasterCraft Computer
+ */
+public class Player implements Serializable
+{
+    private String name;
+    private double bestScore;
+
+    public Player(String name, double bestScore) {
+        this.name = name;
+        this.bestScore = bestScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestScore() {
+        return bestScore;
+    }
+
+    public void setBestScore(double bestScore) {
+        this.bestScore = bestScore;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.bestScore) ^ (Double.doubleToLongBits(this.bestScore) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.bestScore) != Double.doubleToLongBits(other.bestScore)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", bestScore=" + bestScore + '}';
+    }
+    
+    
+    
+    
+}
