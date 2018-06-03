13d3c304039d7fff7361117243f66840be63e1ef
ninjawriter222 <devin.a.poe@gmail.com>
2018-06-02 21:22:35 -0700

    Devin Poe 6/2/18

---
+++ Dungeon Crawler/src/OrgeonTrail/model/AvailableCash.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OrgeonTrail.model;
+
+/**
+ *
+ * @author MasterCraft Computer
+ */
+public class AvailableCash 
+{
+double accountant = 2000;
+double lawer = 1500;
+double farmer = 1000;
+}
