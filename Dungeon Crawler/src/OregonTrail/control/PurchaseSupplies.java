58eb053dd14ce2d948f09303ea1c7f1ef377a8f2
ninjawriter222 <devin.a.poe@gmail.com>
2018-06-02 21:22:27 -0700

    Devin Poe 6/2/18

--- Dungeon Crawler/src/OregonTrail/control/PurchaseSupplies.java
+++ Dungeon Crawler/src/OregonTrail/control/PurchaseSupplies.java
@@ -5,10 +5,53 @@
  */
 package OregonTrail.control;
 
+
+import static OregonTrail.control.PurchaseSupplies.purchase;
+import OrgeonTrail.model.AvailableCash;
 /**
  *
  * @author MasterCraft Computer
  */
-public class PurchaseSupplies {
+public class PurchaseSupplies 
+{
+double AvailableCash;
+double itemAmmount;
+double cost;
+double total = 0;
+double remaingCash;
+
+public void setCash (double AvaliableCash) 
+    {
+    this.setCash(AvaliableCash);
+    }   
+
+public static double purchase(double itemAmmount, double cost)
+    {
+    if(itemAmmount < 0 || itemAmmount > 99)
+        {
+        return -1;
+        }
     
+    double purchase = itemAmmount * cost; 
+    return purchase;
+    }
+
+public static double calculateRemainingCash()
+    {
+    double remainingCash = (AvailableCash - purchase);
+    if (remainingCash < 0 || remainingCash > 2000)
+        {
+        return -1;
+        }
+    else
+        {
+        return RemainingCash;
+        }
+    }
+ 
+
+
+//why is it that I need to define the variable when I return the variable of
+//purchase just before this function?
+
 }
