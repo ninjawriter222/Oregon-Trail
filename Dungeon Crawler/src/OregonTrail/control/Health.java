e8080e5895a8e03b890249486af126e3a0ea8064
Mcvega09 <38776032+Mcvega09@users.noreply.github.com>
2018-06-04 22:48:54 -0400

    ill try this again

--- Dungeon Crawler/src/OregonTrail/control/Health.java
+++ Dungeon Crawler/src/OregonTrail/control/Health.java
@@ -5,11 +5,58 @@
  */
 package OregonTrail.control;
 
+import OrgeonTrail.model.Speed;
+
+
 /**
+o	Health increases or decreases dependent upon the pace.
+	Health can greatly increase if they REST
+
  *
  * @author MasterCraft Computer
  */
-public class Health 
-{
-//save for Vega to work on    
+public class Health {
+
+    private static double playerHealth;
+        
+  public static double checkSpeed(double getSpeed){
+    double speed = getSpeed;
+    
+      if (speed < 1)
+      {
+          return -1;
+      }
+      
+      if (speed > 3)
+      {
+          return -1;
+      }
+      
+      return 1;
+  }
+    
+    public static double checkRest (boolean rest){
+        
+        if (rest = true)
+        {
+            return 20;
+        }
+    
+        return 0;
+    }
+        
+    public static double changeHealth (double speed, int rest){
+        
+        double newSpeed = speed;
+        double newRest = rest;
+        double health = newSpeed + newRest;
+        
+        double newHealth = playerHealth - health;
+        
+        return newHealth;
+        
+        
+        }
 }
+
+
