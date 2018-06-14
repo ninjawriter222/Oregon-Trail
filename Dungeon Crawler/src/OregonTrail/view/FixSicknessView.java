3a17d357cce0c60d22b8bf28d58bb8407789f174
hansm <hansm@DESKTOP-2JJ4RUU>
2018-06-13 18:52:31 -0600

    Finalized GameMenuView. Hans - 6/13

---
+++ Dungeon Crawler/src/OregonTrail/view/FixSicknessView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OregonTrail.view;
+
+/**
+ *
+ * @author hansm
+ */
+class FixSicknessView {
+
+    void displayFixSicknessView() {
+        System.out.println("Calls displayFixSicknessView in FSV");
+    }
+    
+}
