844eab2c197c50bcfcf8cf93e8bf588e95007320
hansm <hansm@DESKTOP-2JJ4RUU>
2018-06-27 17:31:20 -0600

    Added EstimateResources, FixSickness, and HuntResources. Hans - 6/27

--- Dungeon Crawler/src/OregonTrail/view/EstimateResourcesView.java
+++ Dungeon Crawler/src/OregonTrail/view/EstimateResourcesView.java
@@ -9,10 +9,96 @@ package OregonTrail.view;
  *
  * @author hansm
  */
-public class EstimateResourcesView {
+public class EstimateResourcesView extends View {
 
-    void displayEstimateResourcesView() {
-        System.out.println("Calls displayEstimateResources in ERV");
+    public EstimateResourcesView() {
+        super("Which inventory item would you like to review?"
+                + "\nType the letter corresponding to the option/action"
+                + "\nyou want to take."
+                + "\n"
+                + "\nF - Press F to get rid of Food"
+                + "\nA - Press A to get rid of your Axe"
+                + "\nH - Press H to get rid of your Hammer"
+                + "\nS - Press S to get rid of Spare Whell(s)"
+                + "\nB - Press B to get rid of Bullets"
+                + "\nW - Press W to get rid of Wood"
+                + "\nR - Press R to get rid of Blanket(s)"
+                + "\nD - Press D to get rid of Drinking Water"
+                + "\nQ - Press Q to Quit");
+    }
+    
+    @Override
+    public boolean doAction(String inputs) {
+        String menuItem = inputs;
+        switch (menuItem.toUpperCase()) {
+            case "F":
+                getFoodAmt();
+                System.out.println("You have " + "FOOD AMOUNT" + ". How much would you like to leave behind?");
+                return false;
+            case "A":
+                getAxeAmt();
+                System.out.println("You have " + "AXE QUANTITY" + ". How much would you like to leave behind?");
+                return false;
+            case "H":
+                getHammerAmt();
+                System.out.println("You have " + "HAMMER QUANTITY" + ". How much would you like to leave behind?");
+                return false;
+            case "S":
+                getSpareAmt();
+                System.out.println("You have " + "SPARE WHEEL(S) QTY" + ". How much would you like to leave behind?");
+                return false;
+            case "B":
+                getBulletAmt();
+                System.out.println("You have " + "BULLETS" + ". How much would you like to leave behind?");
+                return false;
+            case "W":
+                getWoodAmt();
+                System.out.println("You have " + "WOOD" + ". How much would you like to leave behind?");
+                return false;
+            case "R":
+                getBlanketAmt();
+                System.out.println("You have " + "BLANKETS" + ". How much would you like to leave behind?");
+                return false;
+            case "D":
+                getWaterAmt();
+                System.out.println("You have " + "DRINKING WATER" + ". How much would you like to leave behind?");
+                return false;
+            default:
+                System.out.println("ERROR: Invalid selection. Please try again");
+        }
+        return false;
+    }
+
+    private void getFoodAmt() {
+        System.out.println("***Calls the food qty in the Model Layer***");
+    }
+
+    private void getAxeAmt() {
+        System.out.println("***Calls the axe qty in the Model Layer***");
+    }
+
+    private void getHammerAmt() {
+        System.out.println("***Calls the hammer qty in the Model Layer***");
+    }
+
+    private void getWaterAmt() {
+        System.out.println("***Calls the water qty in the Model Layer***");
+    }
+
+    private void getSpareAmt() {
+        System.out.println("***Calls the spare wheel qty in the Model Layer***");
+    }
+
+    private void getBulletAmt() {
+        System.out.println("***Calls the bullet qty in the Model Layer***");
+    }
+
+    private void getWoodAmt() {
+        System.out.println("***Calls the wood qty in the Model Layer***");
+    }
+
+    private void getBlanketAmt() {
+        System.out.println("***Calls the blanket qty in the Model Layer***");
     }
     
 }
--- Dungeon Crawler/src/OregonTrail/view/FixSicknessView.java
+++ Dungeon Crawler/src/OregonTrail/view/FixSicknessView.java
@@ -11,9 +11,57 @@ import java.util.Scanner;
  *
  * @author hansm
  */
-class FixSicknessView {
+public class FixSicknessView extends View {
 
     public FixSicknessView() {
+        super("Your team has the following ailments:"
+                + "\nAILMENT1"
+                + "\nAILMENT2"
+                + "\nAILMENT3"
+                + "\nAILMENT4"
+                + "\n"
+                + "\nWhich resource would you like to use to heal?"
+                + "\n"
+                + "\nType the letter corresponding to the option/action"
+                + "\nyou want to take."
+                + "\n"
+                + "\nD - Press D to use Drinking Water"
+                + "\nF - Press F to use Food"
+                + "\nR - Press R to use Blankets"
+                + "\nQ - Press Q to quit");
+    }
+    
+    @Override
+    public boolean doAction(String inputs) {
+        String menuItem = inputs;
+        switch (menuItem.toUpperCase()) {
+            case "D":
+                useWater();
+                System.out.println("You have " + "WATER" + ". How much water do you want to use?");
+                return false;
+            case "F":
+                useFood();
+                System.out.println("You have " + "FOOD" + ". How much food do you want to use?");
+                return false;
+            case "R":
+                useBlanket();
+                System.out.println("You have " + "BLANKETS" + ". How many blankets do you want to use?");
+                return false;
+            default:
+                System.out.println("ERROR: Invalid selection. Please try again");
+        }
+        return false;
+    }
 
+    private void useWater() {
+        System.out.println("***Calls the water qty in the Model Layer***");
+    }
+
+    private void useFood() {
+        System.out.println("***Calls the food qty in the Model Layer***");
+    }
+
+    private void useBlanket() {
+        System.out.println("***Calls the blanket qty in the Model Layer***");
     }
 }
