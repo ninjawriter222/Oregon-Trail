7e0c03c24b93b77511f9cc28674f53db4fc23bec
hansm <hansm@DESKTOP-2JJ4RUU>
2018-06-29 16:44:31 -0600

    Working on translating pseudocode from page 5. Hans - 6/29

--- Dungeon Crawler/src/OregonTrail/control/GameControl.java
+++ Dungeon Crawler/src/OregonTrail/control/GameControl.java
@@ -51,6 +51,33 @@ public class GameControl {
 //    }
     
     public static void createNewGame(Player player){
-        System.out.println("***Calls createNewGame method in GC***");
+        
+        public static InventoryItem[] createItems() {
+            System.out.println("***The createItems method has been called***");
+        }
+        
+//    public static int createNewGame(Player player) {
+//        if (player == null)
+//            return -1
+//                    
+//        game = create a new Game object
+//        Save a reference to the Player object in the game
+//        Save a reference to the game in the main class
+//                
+//        actors = createActors()
+//        Save the list of actors in the Game object
+//        Assign an actor to the player
+//                
+//        items = createItems()
+//        Save the list of items in the game
+//                
+//        map = createMap(noOfRows, noOfColumns, items)
+//        IF map == null THEN
+//            RETURN -1
+//        ENDIF
+//                
+//        Assign the map to the game
+//        RETURN 1 // indicates success
+//    }
     }
 }
---
+++ Dungeon Crawler/src/OregonTrail/control/MapControl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OregonTrail.control;
+
+/**
+ *
+ * @author hansm
+ */
+public class MapControl {
+    public static Map createMap(int noOfRows, int noOfColumns) {
+        System.out.println("***The createMap method has been called***");
+    }
+}
